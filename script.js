7f651ae07180368a800061234d74274539b442f6
deepak481 <deepakfeb25@gmail.com>
2023-12-08 14:29:05 +0530

    Created Footer Colon div

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ gsap.to("#main", {
 gsap.from('#about-us,#about-us-in',{
     y: 90,
     opacity: 0,
-    duration: 2,
-    stagger: 0.6,
+    duration: 1.5,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: "#about-us",
       scroller: "body",
@@ -68,13 +68,50 @@ gsap.from(".card", {
     scale: 0.8,
     opacity:0.7,
     duration: 1,
-    stagger: 0.1,
     scrollTrigger: {
       trigger: ".card",
       scroller: "body",
-      // markers:false,
-      start: "top 75%",
-      end: "top 65%",
+    //   markers:true,
+      start: "top 85%",
+      end: "top 75%",
       scrub: 1,
+    },
+  });
+
+gsap.from("#colon1", {
+    y: -50,
+    x: -50,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        // markers: true,
+        start: "top 61%",
+        end: "top 51%",
+        scrub: 4
+    }
+  })
+
+gsap.from("#colon2", {
+    y: 50,
+    x: 50,
+    scrollTrigger: {
+        trigger: "#colon2",
+        scroller: "body",
+        // markers: true,
+        start: "top 100%",
+        end: "top 90%",
+        scrub: 4
+    }
+  })
+
+  gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+    //   markers:true,
+      start: "top 90%",
+      end: "top 85%",
+      scrub: 3,
     },
   });
