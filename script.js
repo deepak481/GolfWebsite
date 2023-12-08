2c38043241af1f5bbd87bbd3f0753088490b0f3c
deepak481 <deepakfeb25@gmail.com>
2023-12-08 09:23:21 +0530

    Completed second page

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ gsap.to("#nav",{
     }
 });
 
+const allNavH5 = document.querySelectorAll("#nav h5");
+
+allNavH5.forEach(elem => {
+    elem.addEventListener("mouseenter", () => {
+        crsr.style.scale = 2;
+        crsr.style.backgroundColor = "transparent";
+        crsr.style.border = "1px solid #95c11e";
+    })
+    elem.addEventListener("mouseleave", () => {
+        crsr.style.scale = 1;
+        crsr.style.backgroundColor = "#95c11e";
+        crsr.style.border = "0px solid #95c11e";
+    })
+})
+
 gsap.to("#main", {
     backgroundColor: "#000",
     scrollTrigger: {
@@ -33,3 +48,33 @@ gsap.to("#main", {
         scrub: 2
     }
 });
+
+gsap.from('#about-us,#about-us-in',{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    stagger: 0.6,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+});
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity:0.7,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".card",
+      scroller: "body",
+      // markers:false,
+      start: "top 75%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
